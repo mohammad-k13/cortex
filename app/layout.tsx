c23a2ea016875cceb9bfc726dae792499a3a9a27
Pouya Khalilian <topnoagency@gmail.com>
2024-04-18 16:28:42 +0330

    fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${poppins.className} flex-center flex-row`}>
+			<body className={poppins.className}>
 				{/* <Header /> */}
-				
 				<main className="max-w-[1950px] max-h-[1080px] mx-auto">{children}</main>
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -20,28 +20,45 @@ export default function Home() {
 	// );
 
 	const ShowToastHandler = async () => {
-		await animate(".button", {scale: 0, zIndex: -10})
-		await animate(".toast-container", {opacity: 1})
-		animate(".toast", {width: 300,height: 200,  opacity: 1, zIndex: 20}, {type: "spring",duration: .56,  damping: 5})
+		await animate(".button", { scale: 0, zIndex: -10 });
+		await animate(".toast-container", { opacity: 1 });
+		animate(
+			".toast",
+			{ width: 300, height: 200, opacity: 1, zIndex: 20 },
+			{ type: "spring", duration: 0.56, damping: 5 },
+		);
 	};
 	const HideToastHandler = async () => {
-		animate(".toast", {width: 0, height: 0,  opacity: 0, zIndex: -10}, {type: "spring",duration: .56,  damping: 5})
-		await animate(".toast-container", {opacity: 0,})
-		animate(".button", {scale: 1, zIndex: 10})
+		animate(
+			".toast",
+			{ width: 0, height: 0, opacity: 0, zIndex: -10 },
+			{ type: "spring", duration: 0.56, damping: 5 },
+		);
+		await animate(".toast-container", { opacity: 0 });
+		animate(".button", { scale: 1, zIndex: 10 });
 	};
 
 	return (
-		<section ref={scope} className="w-full min-h-[100vh] flex-center">
+		<section
+			ref={scope}
+			className="w-full min-h-[100vh] flex-center">
 			<button
 				className="button h-8 px-3 bg-black text-white text-lg rounded-md"
 				onClick={ShowToastHandler}>
 				Click ASSHOLE
 			</button>
 
-			<div  className="toast-container w-full h-full bg-[#33333360] absolute flex-center opacity-0" >
-				<div className="toast w-[300px] h-0 bg-white shadow-ld rounded-xl flex items-center justify-between py-5 flex-col" onClick={HideToastHandler}>
-					<h1 className="text-black w-[70%] text-center">IF I HAD A FACE LIEK YOURS, I'D SUE MY PARENTS.</h1>
-					<p className="text-sm text-gray-500 text-center">poor loser<br /> haaaa</p>
+			<div className="toast-container w-full h-full bg-[#33333360] absolute flex-center opacity-0">
+				<div
+					className="toast w-[300px] h-[100px] bg-white shadow-ld rounded-xl flex items-center justify-between py-5 flex-col"
+					onClick={HideToastHandler}>
+					<h1 className="text-black w-[70%] text-center">
+						IF I HAD A FACE LIEK YOURS, I'D SUE MY PARENTS.
+					</h1>
+					<p className="text-sm text-gray-500 text-center">
+						poor loser
+						<br /> haaaa
+					</p>
 				</div>
 			</div>
 		</section>
