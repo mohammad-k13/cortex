05451d2c775d0e60827b1e9f8c7d47daf3be05d9
Pouya Khalilian <topnoagency@gmail.com>
2024-04-20 16:21:27 +0330

    ui bug related to navbar

--- components/Header/MobileHeader.tsx
+++ components/Header/MobileHeader.tsx
@@ -14,7 +14,7 @@ const ToggleMenu = {
 	open: {
 		right: 0,
 		top: 0,
-		width: "80dvw",
+		width: "80%",
 		height: "100vh",
 		opacity: 1,
 	},
@@ -38,7 +38,7 @@ function MobileHeader() {
 				active={active}
 				setActive={setActive}
 			/>
-			{/* <motion.ul
+			<motion.ul
 				initial={false}
 				className="flex-center flex-col gap-7 absolute top-0 right-0 opacity-0 bg-white z-40"
 				onClick={() => setActive(pv => !pv)}
@@ -48,7 +48,7 @@ function MobileHeader() {
 						<Link href={navItem.path}>{navItem.name}</Link>
 					</li>
 				))}
-			</motion.ul> */}
+			</motion.ul>
 			<div className="flex-center justify-self-start">
 				<Image
 					src={LogoImg}
